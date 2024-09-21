b74741af81382fedbeecc3041a9942441cc2ff16
Juwairyh <juwairyhh12@gmail.com>
2024-09-21 14:02:34 +0700

    Add files via upload

---
+++ Variabel01.java
@@ -0,0 +1,16 @@
+public class Variabel01 {
+    public static void main(String[] args) {
+        // deklarasi var
+        String nama; // tipe_data nama_variabel;
+        int umur;
+
+
+        // input nilai
+        nama = "Juwai"; // nama_variabel = nilai;
+        umur = 17;
+
+        // output ke layar
+        System.out.println("Nama: "+ nama);
+        System.out.println("umur: "+ umur );
+    }
+}
